b694cecd9725767bf3f705792a0ef327a670dc53
D1204416 <161418864+D1204416@users.noreply.github.com>
2024-12-12 15:12:35 +0800

    TopBar

--- sport_frontend/src/App.vue
+++ sport_frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from '@/components/NavBar.vue';
+import HorizontalNav from '@/components/HorizontalNav.vue';
 
 </script>
 
 <template>
   <NavBar/>
+  <HorizontalNav/>
 
 </template>
 
---
+++ sport_frontend/src/components/HorizontalNav.vue
@@ -0,0 +1,91 @@
+<template>
+
+    <div class="container-fluid fs-6">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link link-success active" href="#">首頁</a>
+            </li>
+            <li class="nav-item">
+                <RouterLink to="/AboutView" class="nav-link link-success" aria-current="page">樓層介紹</RouterLink>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link link-success " href="menu.html">場地預約</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link link-success" href="#">交通資訊</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link link-success" href="#">無障礙設施</a>
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+import { RouterLink } from 'vue-router';
+</script>
+
+<style>
+.nav {
+    border-bottom: 2px solid #FF7452 !important; /* 下方加橘色線條 */
+
+}
+.nav-link {
+    color: black !important; /* 非 active 的狀態 */
+    
+}
+.nav-link.active {
+    color: black !important;
+    border-top: 2px solid #FF7452 !important; 
+    border-left: 2px solid #FF7452 !important; 
+    border-right: 2px solid #FF7452 !important; 
+    border-bottom: 1px solid #FF7452 !important;
+}
+@media screen and (max-width: 420px) {
+    .nav-item {
+        font-size: 16px;
+        padding: 3px;
+    }
+
+    .nav-link {
+        padding: 5px 10px;
+    }
+}
+@media screen and (max-width: 395px) {
+    .nav-item {
+        font-size: 14px;
+        padding: 3px;
+    }
+
+    .nav-link {
+        padding: 5px 10px;
+    }
+}
+@media screen and (max-width: 365px) {
+    .nav-item {
+        font-size: 13px;
+        padding: 3px;
+    }
+
+    .nav-link {
+        padding: 5px 10px;
+    }
+}
+@media screen and (max-width: 355px) {
+    .hid {
+        display: none;
+    }
+}
+@media screen and (max-width: 345px) {
+    .nav-item {
+        font-size: 12px;
+        padding: 3px;
+    }
+
+    .nav-link {
+        padding: 5px 10px;
+    }
+}
+/* END */
+</style>
