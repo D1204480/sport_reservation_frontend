c1ee9b8fb8c26f28e09e95683a6d284b54f79e6b
D1204416 <161418864+D1204416@users.noreply.github.com>
2024-12-12 22:22:36 +0800

    分類卡

--- sport_frontend/src/views/ReservationView.vue
+++ sport_frontend/src/views/ReservationView.vue
@@ -1,6 +1,19 @@
-<script></script>
+<script>
+  import SportsFacilities from '../components/SportsFacilities.vue';
+
+  export default {
+    name: 'ReservationView',
+    components: {
+      SportsFacilities
+    }
+  }
+
+</script>
+
 <template>
    <div class="reservation">
     <h1>This is an reservation page</h1>
   </div>
+  
+  <SportsFacilities />
 </template>
