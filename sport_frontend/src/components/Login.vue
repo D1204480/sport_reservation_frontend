0d910af27a2cca4e1fe20270ab0bbb346af51577
D1204416 <161418864+D1204416@users.noreply.github.com>
2024-12-17 09:29:35 +0800

    改Logo

--- sport_frontend/src/components/Login.vue
+++ sport_frontend/src/components/Login.vue
@@ -28,7 +28,7 @@
         <!-- <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 384 512">
           <path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"/>
         </svg> -->
-        <img src="/img/logo.svg" alt="Logo" style="height: 24px;" />
+        <img src="/img/line.png" alt="Logo" style="height: 24px;" />
       </button>
     </div>
 
@@ -535,8 +535,8 @@ body {
 .login-button {
   width: 100%;
   padding: 12px;
-  background-color: #019ee7;
-  border-color: #019ee7;
+  background-color: #FF6242;
+  border-color: #FF6242;
   color: white;
   border: none;
   border-radius: 8px;
@@ -545,6 +545,6 @@ body {
 }
 
 .login-button:hover {
-  background-color: #0074a9;
+  background-color: #ff4f2b;
 }
 </style>
